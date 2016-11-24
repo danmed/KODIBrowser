8c786334a7789bba234130d9b7f285d4fea4b881
danmed <danmed@gmail.com>
2016-11-24 18:35:07 +0000

    Update tvshows.php

--- tvshows.php
+++ tvshows.php
@@ -51,9 +51,8 @@ $poster_path = "posters/" . $imdb . "-3.jpg";
 }
 Else
 {
-$json=file_get_contents("https://thetvdb.com/api/" . $tvdbkey . "/series/" . $imdb);
-$info=json_decode($json, TRUE);
-$poster = $info['poster'];
+$xml = new SimpleXMLElement(file_get_contents("https://thetvdb.com/api/" . $tvdbkey . "/series/" . $imdb);
+$poster = $xml->code->poster;
 $poster_path = " http://www.thetvdb.com/banners/" . $poster;
 file_put_contents($poster, fopen($poster_path, 'r'));
 }
