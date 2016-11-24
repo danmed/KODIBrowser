0658ed51f509c83956183eab47eb26e01d42f1e0
danmed <danmed@gmail.com>
2016-11-24 19:19:11 +0000

    Create tvmenu.php

---
+++ tvmenu.php
@@ -0,0 +1,7 @@
+<br><br>
+<form action="tvshows.php" method="get" target="top">
+<input type="text" name="search" value="search">
+<input type="hidden" name="tag" value="search">
+</form> 
+<br>
+<iframe src="black.htm" name="search" height="50" width="500" frameborder="0" scrolling="no"></iframe>
