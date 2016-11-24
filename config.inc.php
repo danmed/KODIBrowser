5d1b74d1150f617d1a491e7382a551bcd43b7b99
danmed <danmed@gmail.com>
2016-11-24 17:10:12 +0000

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -9,6 +9,9 @@ $password 	= 	'kodi';
 # API key from tmdb.com
 $apikey 	= 	'd0c4af9405af9c13c26e76760839bdfd';
 
+# API key from tvdb.com
+$tvdbkey  =   '78135D1673A5BB8D';
+
 # Internal IP's for "frontroom" and "bedroom" XBMC instances including port numbers
 $xbmc1 		= 	"192.168.1.26:8080";
 $xbmc1label	=	"BR";
