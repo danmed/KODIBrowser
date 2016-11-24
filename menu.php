bbcfe1f72927cc57eb52e90994a4f18c29a44b3a
danmed <danmed@gmail.com>
2016-11-24 15:09:52 +0000

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,4 +1,4 @@
 <br><br>
-<font face="arial" size="3">< <a href="search2.php?type=genre"">GENRE</a> | <a href="search2.php?type=rate">RATING</a> | <a href="search2.php?type=year">YEAR</a> | <a href="search2.php?type=movie">SEARCH</a> ></font>
+<font face="arial" size="3">< <a href="search2.php?type=genre" target="search">GENRE</a> | <a href="search2.php?type=rate" target="search">RATING</a> | <a href="search2.php?type=year" target="search">YEAR</a> | <a href="search2.php?type=movie" target="search">SEARCH</a> ></font>
 <br>
 <iframe src="black.htm" name="search" height="50" width="500" frameborder="0" scrolling="no"></iframe>
