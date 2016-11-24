02f702ab1cc8ca670dd07e9e9321494be4c18212
danmed <danmed@gmail.com>
2016-11-24 18:44:32 +0000

    Update tvshows.php

--- tvshows.php
+++ tvshows.php
@@ -11,29 +11,7 @@
 </font><br><br>
 <?PHP
 include "config.inc.php";
-    function objectsIntoArray($arrObjData, $arrSkipIndices = array())
-{
-    $arrData = array();
 
-    // if input is object, convert into array
-    if (is_object($arrObjData)) {
-        $arrObjData = get_object_vars($arrObjData);
-    }
-
-    if (is_array($arrObjData)) {
-        foreach ($arrObjData as $index => $value) {
-            if (is_object($value) || is_array($value)) {
-                $value = objectsIntoArray($value, $arrSkipIndices); // recursive call
-            }
-            if (in_array($index, $arrSkipIndices)) {
-                continue;
-            }
-            $arrData[$index] = $value;
-        }
-    }
-    return $arrData;
-}
-?>
 $searchstring = $_GET["search"];
 $tag = $_GET["tag"];
 $db_handle = mysql_connect($server, $username, $password);
