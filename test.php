58bf82de13d337edc03a9d01c9cb0e1d4eec0fba
danmed <danmed@gmail.com>
2016-11-28 12:43:31 +0000

    Update test.php

--- test.php
+++ test.php
@@ -5,17 +5,15 @@ $episodeid = $_GET["ep"];
 $db_handle = mysql_connect($server, $username, $password);
 $db_found  = mysql_select_db($database, $db_handle);
 if ($db_found) {
-$SQL3    = "select c01,c00 from episode where idepisode = '" . $episodeid . "' AND idshow = '" . $showid . "'";
-  $result  = mysql_query($SQL3);
-
+$SQL3    = "select c01,c00 from episode where idepisode = '" . $episodeid . "' AND idshow = '" . $showid . "' LIMIT 1";
+$result  = mysql_query($SQL3);
+  
                                while ( $db_field = mysql_fetch_array($result));
                                {
                                 $episodedescription = $db_field['c01'];
                                 $episodetitle       = $db_field['c00'];
                                }
 print $SQL3;
-while($row = mysql_fetch_object($result))
-    var_dump($row);
 print $episodedescription;
 print $episodetitle;
 }
