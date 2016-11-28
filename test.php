5bd0443783a9d471b6af0e657391f95bfa375e77
danmed <danmed@gmail.com>
2016-11-28 12:50:48 +0000

    Update test.php

--- test.php
+++ test.php
@@ -8,9 +8,7 @@ if ($db_found) {
 $SQL3    = "select c01,c00 from episode where idepisode = '" . $episodeid . "' AND idshow = '" . $showid . "' LIMIT 1";
 $result  = mysql_query($SQL3);
   
-                                $value = mysql_result($result, i, 'c01;
-                                $episodedescription = $db_field['c01'];
-                                $episodetitle       = $db_field['c00'];
+                                $value = mysql_result($result, 0, 'c01;
                                
 print "SQL : " . $SQL3 . "<br>";
 print "ROWs : " . mysql_num_rows($result) . "<br>";
