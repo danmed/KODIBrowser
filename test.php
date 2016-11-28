996fe1edb88e6cfbdb572b419fa298a6a9c1a9f1
danmed <danmed@gmail.com>
2016-11-28 12:35:52 +0000

    Update test.php

--- test.php
+++ test.php
@@ -7,8 +7,9 @@ $db_handle = mysql_connect($server, $username, $password);
 $db_found  = mysql_select_db($database, $db_handle);
 if ($db_found) {
 $SQL3    = "select c01,c00 from episode where idepisode = '" . $episodeid . "' AND idshow = '" . $showid . "'";
+  $result  = mysql_query($SQL3);
 
-                               while ( $db_field = mysql_fetch_assoc($result3));
+                               while ( $db_field = mysql_fetch_assoc($result));
                                {
                                 $episodedescription = $db_field['c01'];
                                 $episodetitle       = $db_field['c00'];
