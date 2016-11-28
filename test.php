30c855b1f6e28dfba9050e5a66af3f5deefb65b0
danmed <danmed@gmail.com>
2016-11-28 12:26:11 +0000

    Update test.php

--- test.php
+++ test.php
@@ -7,15 +7,11 @@ $db_handle = mysql_connect($server, $username, $password);
 $db_found  = mysql_select_db($database, $db_handle);
 if ($db_found) {
 $SQL3    = "select c01,c00 from episode where idepisode = '" . $episodeid . "' AND idshow = '" . $showid . "'";
-$result3  = mysql_query($SQL3);
-  if (!$result3) {
-    echo 'Could not run query: ' . mysql_error();
-    exit;
-}
+
                                $episode_info = mysql_fetch_row($result3); 
                                 $episodedescription = $episode_info['c01'];
                                 $episodetitle       = $episode_info['c00'];
                                 
 print $episodedescription;
-print episodetitle;
+print $episodetitle;
 }
