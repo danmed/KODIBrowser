60d1d385cca93c9538a16485e17657e41a1295ef
danmed <danmed@gmail.com>
2016-11-29 11:32:40 +0000

    Update search2.php

--- search2.php
+++ search2.php
@@ -1,27 +1,23 @@
 <?PHP
 $type = $_GET["type"];
-if ($type == "genre") 
-{
-$tag = "genre";
+if ($type == "genre") {
+    $tag = "genre";
+} elseif ($type == "rate") {
+    $tag = "rate";
+} elseif ($type == "year") {
+    $tag = "year";
+} else {
+    $tag = "movie";
 }
-elseif ($type == "rate")
-{
-$tag = "rate";
-}
-elseif ($type == "year")
-{
-$tag = "year";
-}
-else
-{
-$tag = "movie";
-}
-
 ?>
 
 <body bgcolor="black">
 <center>
 <form action="index.php" method="get" target="top">
-<input type="text" name="search" value="<?PHP print $tag; ?>">
-<input type="hidden" name="tag" value="<?PHP print $tag; ?>">
+<input type="text" name="search" value="<?PHP
+print $tag;
+?>">
+<input type="hidden" name="tag" value="<?PHP
+print $tag;
+?>">
 </form> 
