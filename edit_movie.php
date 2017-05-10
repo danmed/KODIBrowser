99c3bf063284c879839e44248b9d7aff6962bc66
danmed <danmed@gmail.com>
2017-05-10 11:03:37 +0100

    Update edit_movie.php

--- edit_movie.php
+++ edit_movie.php
@@ -19,7 +19,7 @@ if ($db_found) {
      $movie_studio = mysqli_real_escape_string($_POST['studio']);
                     $sql = "UPDATE movie SET c00 = '" . $movie_title . "', c01 = '" . $movie_synopsis . "', c03 = '" . $movie_tagline . "', c12 = '" . $movie_rating . "', c14 = '" . $movie_genres . "', c15 = '" . $movie_director . "', c18 = '" . $movie_studio . "' WHERE idmovie = '" . $idmovie . "'" ;
 
-            $retval = mysqli_query($sql);
+            $retval = mysqli_query($db_handle, $sql);
             
             if(! $retval ) {
                die('Could not update data: ' . mysql_error());
