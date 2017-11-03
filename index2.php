09a529822f207f91c2d53566531ec19cf52fd008
danmed <danmed@gmail.com>
2017-11-03 16:03:42 +0000

    Update index2.php

--- index2.php
+++ index2.php
@@ -1,20 +1,57 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
-<head>
-<title> Kodi Browser </title>
-        <link rel="stylesheet" title="Standard" href="styles.css" type="text/css" media="screen" />
-</head>
-<body>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.15/css/dataTables.bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.1.1/css/responsive.bootstrap.min.css">
+    <style type="text/css" class="init">
+    
+    </style>
+    <script type="text/javascript" src="/media/js/site.js?_=45ee69f7580387099dcc5163940d7394">
+    </script>
+    <script type="text/javascript" src="/media/js/dynamic.php?comments-page=extensions%2Fresponsive%2Fexamples%2Fstyling%2Fbootstrap.html" async>
+    </script>
+    <script type="text/javascript" language="javascript" src="//code.jquery.com/jquery-1.12.4.js">
+    </script>
+    <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/1.10.15/js/jquery.dataTables.min.js">
+    </script>
+    <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/1.10.15/js/dataTables.bootstrap.min.js">
+    </script>
+    <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/responsive/2.1.1/js/dataTables.responsive.min.js">
+    </script>
+    <script type="text/javascript" language="javascript" src="https://cdn.datatables.net/responsive/2.1.1/js/responsive.bootstrap.min.js">
+    </script>
+
+    <script src="js/bootstrap.min.js"></script>     
+
+    <script type="text/javascript" class="init">
+    
+$(document).ready(function() {
+    $('#status').DataTable({  "pageLength": <?php echo $row_count; ?>, stateSave: true });
+} );
+    </script>
+<meta http-equiv="refresh" content="<?PHP echo $refresh; ?>">
+    <title>PStatus</title>
+
+<style>    
+  .progress {
+    margin-bottom: 0 !important;
+    background-color: #DA2A2A;
+    -webkit-box-shadow: none;
+    box-shadow: none;
+}
+</style>
+
+    </head>
+
+  <body>
+
+    <?PHP
+include "navbar.php";
+?>
 <center>
-<table border=0 cellpadding=0 cellspacing=0 width=100%>
-    <tr>
-        <td width=33%></td>
-        <td width=33%><font face="arial" size="15">KODI BROWSER</font></td>
-        <Td align=right width=33%><a href="tvshows.php"><img src="images/tvshows.png" border="0" width="30"></a></td>
-    </tr>
-</table>
-</font><br><br>
 <?PHP
 include "config.inc.php";
 $searchstring = $_GET["search"];
